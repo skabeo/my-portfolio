cb4d581d8f0178d57c1e13ecf988e72120f5f8ee
skabeo <spencerokyere95@gmail.com>
2023-03-06 13:29:39 +0000

    Add functionality to the menu icon

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  menu.style.display = 'initial';
+})
