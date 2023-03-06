0fb608897157200b826df9ba8a1fca02fb0aa8d6
skabeo <spencerokyere95@gmail.com>
2023-03-06 18:15:36 +0000

    modify pop up menu

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeButton = document.querySelector('.close-btn');
+const links = document.querySelectorAll('.links a');
 
 hamburger.addEventListener('click', () => {
   menu.style.display = 'block';
@@ -10,8 +11,11 @@ closeButton.addEventListener('click', () => {
   menu.style.display = 'none';
 })
 
-menu.addEventListener('click',(event) =>{
-  if (event.target.tagName === 'A') {
-    menu.style.display = 'none'
-  }
-})
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    menu.style.display = 'none';
+    const targetSection = document.querySelector(event.target.hash);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+    event.preventDefault();
+  });
+});
